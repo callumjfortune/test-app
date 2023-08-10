50f9db16eacac6e4988cbcf29b0c90d2e94ddefb
callumjfortune <callum.fortune@bbc.co.uk>
2023-08-10 14:24:03 +0100

    rust stuff

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+
+    let mut current_day = "Thursday";
+    let mut days_remaining = 365;
+    let mut next_day;
+
+    while days_remaining > 0 {
+        next_day = match current_day {
+            "Monday" => "Tuesday",
+            "Tuesday" => "Wednesday",
+            "Wednesday" => "Thursday",
+            "Thursday" => "Friday",
+            "Friday" => "Saturday",
+            "Saturday" => "Sunday",
+            "Sunday" => "Monday",
+            _ => ""
+        };
+
+        current_day = next_day;
+        println!("The next day after that will be {}", current_day);
+        days_remaining -= 1;
+
+        
+
+    }
+
+}
